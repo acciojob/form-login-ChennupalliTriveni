2062ff70fb66e7f4e38138a4324b58a7a98709af
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-11-20 19:03:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+  event.preventDefault(); // prevent page refresh
 
+  // Get values and trim spaces
+  let first = document.forms["myForm"]["fname"].value.trim();
+  let last = document.forms["myForm"]["lname"].value.trim();
+
+  // Full name
+  let fullName = first + " " + last;
+
+  // Show alert
+  alert(fullName);
 }
